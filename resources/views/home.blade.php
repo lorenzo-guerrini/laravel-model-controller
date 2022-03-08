2ea36c2bd11f3a16e89420f022378df7e96dcfee
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-03-08 02:20:22 +0100

    Punto 7 completato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movies</title>
 </head>
+
 <body>
     <h1>Movies</h1>
-    @dump($moviesList);
+    <ul>
+        @foreach ($moviesList as $item)
+            <li>
+                {{ $item->title }} ({{$item->original_title}})
+                <ul>
+                    <li>Nationality: {{ $item->nationality }}</li>
+                    <li>Date: {{ $item->date }}</li>
+                    <li>Vote: {{ $item->vote }}</li>
+                </ul>
+            </li>
+        @endforeach
+    </ul>
 </body>
+
 </html>
